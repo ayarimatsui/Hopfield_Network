1a9d939ab1d6a06cc891c739cf7376c70c2293df
ayarimatsui <matsui-ayari682@g.ecc.u-tokyo.ac.jp>
2020-06-17 03:17:46 +0900

    updated & modified

--- main.py
+++ main.py
@@ -387,10 +387,113 @@ def main5():
     plt.savefig("figures/figure5.png")
     
 
+##################### ここからは追加実験 ####################
+
+# ノイズが50%を越える時(76%)の時の元画像、入力画像、出力画像を表示
+def with_big_noise():
+    num_of_trials = 100  # 試行回数は100回とする
+    noise = int(25 * 0.76)   # 76%のノイズを加える
+    similarity_sum = 0
+    correct_sum = 0
+    # 画像描画用
+    original_list = []
+    noise_added_list = []
+    recollected_list = []
+    for i in range(num_of_trials):
+        hopfield = Hopfield_Network()
+        image = create_images(1)
+        hopfield.train(image)   # 元画像を記憶させる
+        noise_added_img = add_noise(image, noise)
+        recollected_img = hopfield.recollect(noise_added_img)
+        similarity, correct = check(image, recollected_img)
+        similarity_sum += similarity / num_of_trials
+        correct_sum += correct / num_of_trials
+        # 最後の5回の試行の時のみ、元画像、ノイズを加えた画像、想起された画像を表示したいのでリストに保存しておく
+        if i >= num_of_trials - 5:
+            original_list.append(image)
+            noise_added_list.append(noise_added_img)
+            recollected_list.append(recollected_img)
+    # 描画
+    fig = plot(original_list, noise_added_list, recollected_list)
+    plt.text(-15, -27, 'Number of Types: 1   Noise Rate: 76%', fontsize=13)
+    plt.text(-10, 6.5, 'similarity: {:.1f},     accuracy: {:.1f}'.format(similarity_sum, correct_sum * 100))
+    plt.savefig("figures/figure6.png")
+    print("類似度 : {:.1f},   正答率 : {:.1f}".format(similarity_sum, correct_sum * 100))
+
+
+# 実験3と同様の実験下(画像の種類1~6, ノイズ0~100%)で、元画像の反転画像との類似度、正答率を計算する
+def check_inversion():
+    num_of_images = [n for n in range(1, 7)]  # 画像の種類は1~6種類
+    num_of_trials = 100  # 試行回数は100回とする
+    noise_list = [i for i in range(0, 101, 4)]   # 0~100%まで4%ずつノイズを増やす
+    total_sim_list = []
+    total_acc_list = []
+    total_sim_list_inv = []
+    total_acc_list_inv = []
+    for n in num_of_images:
+        sim_list = []
+        acc_list = []
+        sim_list_inv = []
+        acc_list_inv = []
+        for noise_percentage in noise_list:
+            similarity_sum = 0
+            correct_sum = 0
+            similarity_sum_inv = 0
+            correct_sum_inv = 0
+            for i in range(num_of_trials):
+                hopfield = Hopfield_Network()
+                image = create_images(n)  # n個の画像を生成
+                hopfield.train(image)   # 元画像を記憶させる
+                for j in range(n):
+                    target_image = image[j]
+                    noise = int(25 * noise_percentage / 100)   # ノイズを加える
+                    noise_added_img = add_noise(target_image, noise)
+                    recollected_img = hopfield.recollect(noise_added_img)
+                    # 想起された画像(出力を反転)
+                    recollected_img_inv = -1 * recollected_img
+                    similarity, correct = check(target_image, recollected_img)
+                    similarity_inv, correct_inv = check(target_image, recollected_img_inv)
+                    similarity_sum += similarity / (n * num_of_trials)
+                    correct_sum += correct / (n * num_of_trials)
+                    similarity_sum_inv += similarity_inv / (n * num_of_trials)
+                    correct_sum_inv += correct_inv / (n * num_of_trials)
+            
+            sim_list.append(similarity_sum)
+            acc_list.append(correct_sum * 100)
+            sim_list_inv.append(similarity_sum_inv)
+            acc_list_inv.append(correct_sum_inv * 100)
+
+        total_sim_list.append(sim_list)
+        total_acc_list.append(acc_list)
+        total_sim_list_inv.append(sim_list_inv)
+        total_acc_list_inv.append(acc_list_inv)
+    
+    # グラフの描画
+    fig, axes = plt.subplots(3, 2, figsize=(15, 10))  
+    for i in range(len(num_of_images)):
+        axes.flat[i].plot(noise_list, total_sim_list[i], label='similarity', color='r')
+        axes.flat[i].plot(noise_list, total_acc_list[i], label='accuracy', color='g')
+        axes.flat[i].plot(noise_list, total_sim_list_inv[i], label='similarity with inverted image', color='violet')
+        axes.flat[i].plot(noise_list, total_acc_list_inv[i], label='accuracy with inverted image', color='springgreen')
+        axes.flat[i].set_xlabel("noise rate (%)")
+        axes.flat[i].set_xlim(0, 100)
+        axes.flat[i].set_ylabel("similarity and accuracy (%)")
+        axes.flat[i].set_ylim(0, 101)
+        axes.flat[i].set_title("Similarity, Accuracy and Noise (image types: {})".format(i+1))
+    axes.flat[0].legend(loc='lower center', bbox_to_anchor=(1.1, 1.2), ncol=4)
+
+    # 余白を設定
+    plt.subplots_adjust(wspace=0.4, hspace=0.6)
+    # グラフを保存
+    plt.savefig("figures/figure7.png")
+
+
 
 if __name__ == '__main__':
-    main1()
-    main2()
-    main3()
-    main4()
-    main5()
+    #main1()
+    #main2()
+    #main3()
+    #main4()
+    #main5()
+    #with_big_noise()
+    #check_inversion()
